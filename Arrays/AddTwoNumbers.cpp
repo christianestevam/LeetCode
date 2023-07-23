ebe9cc05f29d2f0ebf9c488cc50871068e098bf8
Christian Estevam <christianestevamb@gmail.com>
2023-07-23 16:20:27 -0300

    AddTwoTwoNumbers

---
+++ Arrays/AddTwoNumbers.cpp
@@ -0,0 +1,66 @@
+/*
+You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+Example 2:
+
+Input: l1 = [0], l2 = [0]
+Output: [0]
+Example 3:
+
+Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+Output: [8,9,9,9,0,0,0,1]
+
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+*/
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include "ListNode.h"
+
+struct ListNode {
+      int val;
+  ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+      ListNode(int x) : val(x), next(nullptr) {}
+      ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* result = new ListNode();
+        ListNode* current = result;
+        int carry = 0;
+        while(l1 != nullptr || l2 != nullptr) {
+            int sum = carry;
+            if(l1 != nullptr) {
+                sum += l1->val;
+                l1 = l1->next;
+            }
+            if(l2 != nullptr) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+            carry = sum / 10;
+        }
+        if(carry > 0) {
+            current->next = new ListNode(carry);
+        }
+        return result->next;
+    }
+};
